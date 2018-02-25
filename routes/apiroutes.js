5461ad582d4c7eae3eedd099aabf195096a5c084
Travis Roemhild <travis.roemhild@gmail.com>
2018-02-25 13:22:33 -0700

    done

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -7,5 +7,25 @@ module.exports = function(app) {
         });
     })
     //front end ajax is done, need to create the rest of the backend routes
-    app.put('')
+    app.post('/api/burgers',function(req,res){
+    	db.Burger.create({
+    		burger_name:req.body.burger_name,
+    		devoured:req.body.devoured
+    	}).then(function(data){
+    		res.json(data);
+    	});
+    });
+
+    app.put('/api/burgers/:id',function(req,res){
+    	db.Burger.update({
+    		devoured:1
+    	},
+    	{
+    		where:{
+    			id:req.params.id
+    		}
+    	}).then(function(data){
+    		res.json(data)
+    	})
+    })
 }
